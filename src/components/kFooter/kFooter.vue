c13882d3af654787aa98cf794abdb19aad13a93d
venskaya <venskaya@kodeks.ru>
2017-02-09 15:02:12 +0300

    add footer

---
+++ src/components/kFooter/kFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12 col-md-10 col-sm-offset-0 col-md-offset-1">
+          <div class="footer_copyright">
+            <slot>
+              <div class="footer_copyright_tx">&copy;&nbsp;АО&nbsp;&laquo;Кодекс&raquo;, 2017</div>
+							<div class="footer_copyright_tx __margin-top_xs">Исключительные авторские и&nbsp;смежные права принадлежат АО&nbsp;&laquo;Кодекс&raquo;.</div>
+            </slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type="text/javascript">
+    export default {}
+
+</script>
+
+<style lang="sass" src="./style.sass">
+
+</style>
